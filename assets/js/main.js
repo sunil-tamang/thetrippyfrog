2a09f5ade18412d88c73f50b7eaa900445a427e7
arjun0108 <arjunthakur01081997@gmail.com>
2019-04-05 21:50:33 +0530

    New Changes as suggested

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,31 @@
 
    
 $(document).ready(function(){
+	// Gallery
+	 if($('.image-popup-vertical-fit').length){
+		$('.image-popup-vertical-fit').magnificPopup({
+		  type: 'image',
+		  mainClass: 'mfp-with-zoom', 
+		  gallery:{
+			  enabled:true
+			},
+
+		  zoom: {
+			enabled: true, 
+
+			duration: 300, // duration of the effect, in milliseconds
+			easing: 'ease-in-out', // CSS transition easing function
+
+			opener: function(openerElement) {
+
+			  return openerElement.is('img') ? openerElement : openerElement.find('img');
+		  }
+		}
+
+		});
+	  }
+
+	
 	if($('.trek-slider').length){
 			$('.trek-slider').slick({
 				touchMove:true,
