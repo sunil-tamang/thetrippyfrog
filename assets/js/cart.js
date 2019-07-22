a7fe5fea8997be3fa45d5009de4fb77072909686
arjun0108 <arjunthakur01081997@gmail.com>
2019-07-22 23:46:01 +0530

    Added Bike Tours

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -361,6 +361,36 @@ business_paypal = 'jondoe@gmail.com'; // here goes your paypal email
 				urlName : 'magic-valley-escape'
 			},
 
+			
+			//Bike-Tours Products
+			
+			{
+				id : 24,
+				name : 'Leh-Manali Bike Tour',
+				img : 'assets/css/images/bike-tours/leh-manali/leh-manali_min.jpg',
+				price : 50000.00,
+				prodType : 'bike-tour',
+				stock : 1,
+				days : 10,
+				nights: 9,
+				fixedDate: '15 September',
+				urlName : 'leh-manali-bike-tour'
+			},
+			
+			{
+				id : 25,
+				name : 'Spiti Bike Tour',
+				img : 'assets/css/images/bike-tours/spiti/spiti_min.jpg',
+				price : 40000.00,
+				prodType : 'bike-tour',
+				stock : 1,
+				days : 8,
+				nights: 7,
+				fixedDate: '8 August',
+				urlName : 'spiti-bike-tour'
+			},
+
+			
 			{
 				id : 24,
 				name : 'Shimla-Manali Tour',
@@ -394,6 +424,10 @@ business_paypal = 'jondoe@gmail.com'; // here goes your paypal email
 		else if(productName=='escape-trips'){
 			lowerLimit = 21
 			upperLimit = 23
+		}
+		else if(productName=='bike-tours'){
+			lowerLimit = 24
+			upperLimit = 25
 		}
 		else{
 			lowerLimit = 18
@@ -526,6 +560,43 @@ business_paypal = 'jondoe@gmail.com'; // here goes your paypal email
 //					content+= '</div>'
 //					content+= '</div>'
 //					content+= '</div>'
+				}
+			}
+			
+			// Bike-Tours
+			else if((upperLimit >= 23)&&(upperLimit<=25)){
+				if(products[i].stock > 0){
+
+					content+='<div class="row--padding-medium col-medium">'
+					content+='<div class="trek-slide">'
+					content+='	<div style="position: relative;">'
+					content+='		<img class="trek-slide--img trek-slide4" style="background-image:url(/'+ products[i].img +')">'
+					content+='		<div class="wopper__footer adventure">'
+					content+='			<div class="wopper__info" style="padding: 0;">'
+					content+='				<a class="wopper__info-price white wopper__act-name" href="/activities/bike-tours/'+ products[i].urlName +'"><span>'+ products[i].name +'</span></a>'
+					content+='			</div>'
+					content+='		</div>'
+					content+='	</div>'
+					content+='	  <span class="trek-slide--price"><span style="font-size: 60%;">From</span> <i class="fa fa-rupee-sign"></i> '+ products[i].price +'/-</span>'
+					content+= '<button class="wopper__info-cart btn-escape btn prod-'+products[i].id+'" data-style="zoom-out" onclick="app.addtoCart('+products[i].id+');" title="Add To Cart"><i class="fas fa-cart-plus"></i></button>'
+					content+='	<div class="trek-slide--text">'
+					content+='		<div class="trek-slide--meta">'
+					content+='			<div class="">'
+					content+='				<span>'+ products[i].days +'</span>'
+					content+='				<span class="meta-type">DAYS &nbsp;<i class="fas fa-sun"></i></span>'
+					content+='			</div>'
+					content+='			<div class="">'
+					content+='				<span>'+ products[i].nights +'</span>'
+					content+='				<span class="meta-type">NIGHTS &nbsp;<i class="fas fa-moon"></i></span>'
+					content+='			</div>'
+					content+='			<div class="">'
+					content+='				<span>'+ products[i].fixedDate +'</span>'
+					content+='				<span class="meta-type">DEPARTURE &nbsp;<i class="fas fa-calendar-alt"></i></span>'
+					content+='			</div>'
+					content+='		</div>'
+					content+='	</div>'
+					content+='</div>'
+					content+='</div>'
 				}
 			}
 
